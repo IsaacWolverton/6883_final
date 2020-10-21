429cc2ab2c9e9ea80de36050794b4fa5064db07c
Anisha Agarwal <anisha24@mit.edu>
2020-10-20 23:58:07 -0400

    scaffold for non-ml hyperparameter search

---
+++ option_keyboard/gpe_gpi_experiments/hp_optimizers.py
@@ -0,0 +1,30 @@
+# file with all the classes for different hyperparameter tuning
+# TODO: JULIA - implement EvolutionTuning
+#       ANISHA - implement BayesianOptimization
+
+import random
+
+class HyperParamTuning:
+    
+    def __init__(self, range_min, range_max):
+        self.range_min = range_min
+        self.range_max = range_max
+
+    def next_value(self):
+        pass
+    
+
+class RandomSearch(HyperParamTuning):
+
+    def next_value(self):
+        return random.uniform(self.range_min, self.range_max)
+
+
+class EvolutionTuning(HyperParamTuning):
+    def next_value(self):
+        pass
+
+        
+class BayesianOptimization(HyperParamTuning):
+    def next_value(self):
+        pass
