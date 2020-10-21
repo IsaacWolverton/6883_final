18b9de7f3e1f859f07942a3b5211a903a62334c1
Julia Wagner <jnwagner@mit.edu>
2020-10-21 03:21:28 -0400

    Add GridSearch draft to HyperParamTuning. Add some TODOs and notes on EvolutionTuning.

--- option_keyboard/gpe_gpi_experiments/hp_optimizers.py
+++ option_keyboard/gpe_gpi_experiments/hp_optimizers.py
@@ -4,10 +4,12 @@
 
 import random
 
+# is this for individual parameters?
 class HyperParamTuning:
     
     def __init__(self, range_min, range_max):
         self.range_min = range_min
+        # TODO: note whether max is inclusive
         self.range_max = range_max
 
     def next_value(self):
@@ -19,8 +21,25 @@ class RandomSearch(HyperParamTuning):
     def next_value(self):
         return random.uniform(self.range_min, self.range_max)
 
+class GridSearch(HyperParamTuning):
+    def __init__(self, range_min, range_max, number_of_splits):
+        super().__init__(range_min, range_max)
+        step = (range_max - range_min)/number_of_splits
+        self.values = iter([range_min + i * step for i in range(number_of_splits+1)])
+
+    def next_value(self):
+        # TODO: decide where to handle stop iteration error
+        return next(self.values)
 
 class EvolutionTuning(HyperParamTuning):
+    def __init__(self, range_min, range_max, population_size):
+        super().__init__(range_min, range_max)
+        self.values = [random.uniform(self.range_min, self.range_max) for i in range(population_size)]
+
+    '''
+    Once we've evaluated all of population, top X% will crossover to make offspring, and some mutations
+    are introduced.
+    '''
     def next_value(self):
         raise NotImplementedError
 
