94510b87f314af6518afe51c07f7d2dc37dcb2b8
Julia Wagner <jnwagner@mit.edu>
2020-10-26 20:44:35 -0400

    Altered structure of HyperParamTuning class to have a method for each type of tuning instead of a subclass for each. Implemented tuning methods that call the model evaluation method and output the best parameters. Added docstrings.

--- option_keyboard/gpe_gpi_experiments/hp_optimizers.py
+++ option_keyboard/gpe_gpi_experiments/hp_optimizers.py
@@ -1,49 +1,62 @@
 # file with all the classes for different hyperparameter tuning
-# TODO: JULIA - implement EvolutionTuning
-#       ANISHA - implement BayesianOptimization
-
 import random
+from skopt import gp_minimize
 
-# is this for individual parameters?
+'''
+This class is for tuning hyperparameters. It takes in an evaluate model which should be a method
+that takes in a list of hyperparameters and outputs the objective value. Each method will run
+tuning using the corresponding type and return the optimal hyperparameters in a list. Note that
+each method will thus run evaluate_model multiple times in order to evaluate several different
+hyperparameter possibilities. 
+'''
 class HyperParamTuning:
     
-    def __init__(self, range_min, range_max):
-        self.range_min = range_min
-        # TODO: note whether max is inclusive
-        self.range_max = range_max
+    def __init__(self, evaluate_model):
+        self.evaluate_model = evaluate_model    
 
-    def next_value(self):
-        raise NotImplementedError("Implement this function when you extend the class")
-    
+    '''
+    This performs random search using the evaluate model and returns a list of the params that 
+    evaluate to the highest objective value.
+    range_min (float): lower, inclusive bound of the range to random search
+    range_max (float): upper, inclusive bound of the range to random search
+    iterations (integer): number of times to evaluate on the random parameters 
+    '''
+    def tuneWithRandomSearch(self, range_min, range_max, iterations):
+        best_objective_val = None
+        best_params = None
+        for i in range(iterations):
+            params = [random.uniform(range_min, range_max)]
+            current_objective_val = self.evaluate_model([params])
+            if not best_objective_val or current_objective_val > best_objective_val:
+                best_params = params
+                best_objective_val = current_objective_val  
+        return best_params
 
-class RandomSearch(HyperParamTuning):
-
-    def next_value(self):
-        return random.uniform(self.range_min, self.range_max)
-
-class GridSearch(HyperParamTuning):
-    def __init__(self, range_min, range_max, number_of_splits):
-        super().__init__(range_min, range_max)
+    '''
+    This performs grid search using the evaluate model and returns a list of the params that 
+    evaluate to the highest objective value.
+    range_min (float): lower, inclusive bound of the range to random search
+    range_max (float): upper, inclusive bound of the range to random search
+    number_of_splits (integer): number of times to evaluate on the parameters, in 
+                                (range_max-range_min)/number_of_splits increments
+    '''
+    def tuneWithGridSearch(self, range_min, range_max, number_of_splits):
         step = (range_max - range_min)/number_of_splits
-        self.values = iter([range_min + i * step for i in range(number_of_splits+1)])
-
-    def next_value(self):
-        # TODO: decide where to handle stop iteration error
-        return next(self.values)
-
-class EvolutionTuning(HyperParamTuning):
-    def __init__(self, range_min, range_max, population_size):
-        super().__init__(range_min, range_max)
-        self.values = [random.uniform(self.range_min, self.range_max) for i in range(population_size)]
+        best_objective_val = None
+        best_params = None
+        for i in range(number_of_splits+1):
+            params = [range_min + i * step]
+            current_objective_val = self.evaluate_model([params])
+            if not best_objective_val or current_objective_val > best_objective_val:
+                best_params = params
+                best_objective_val = current_objective_val  
+        return best_params
 
     '''
-    Once we've evaluated all of population, top X% will crossover to make offspring, and some mutations
-    are introduced.
+    This performs bayesian optimization using the evaluate model and returns a list of the params that 
+    evaluate to the highest objective value.
+    range_min (float): lower, exclusive bound of the range to random search
+    range_max (float): upper, exclusive bound of the range to random search
     '''
-    def next_value(self):
-        raise NotImplementedError
-
-        
-class BayesianOptimization(HyperParamTuning):
-    def next_value(self):
-        raise NotImplementedError
+    def tuneWithBayesianOptimization(self, range_min, range_max):
+        return gp_minimize(self.evaluate_model, [float(range_min), float(range_max)])
