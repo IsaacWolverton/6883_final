07920cec9ec6cce74fa514662d712ad81a0937c1
Julia Wagner <jnwagner@mit.edu>
2020-10-27 23:56:00 -0400

    Update requirements. Add evaluate_dqn to test.

--- option_keyboard/gpe_gpi_experiments/test.py
+++ option_keyboard/gpe_gpi_experiments/test.py
@@ -1,4 +1,5 @@
-from hp_optimizers import HyperParamTuner
+from option_keyboard.gpe_gpi_experiments.hp_optimizers import HyperParamTuner
+from option_keyboard.run_hyperparameter_search import evaluate_dqn
 
 def my_func(params):
     val = params[0]
@@ -6,8 +7,9 @@ def my_func(params):
 
 if __name__ == "__main__":
     hyperParamTuner = HyperParamTuner(my_func)
-    range_min = -1
+    range_min = 0
     range_max = 1
-    print (hyperParamTuner.tuneWithRandomSearch(range_min, range_max, 10))
-    print (hyperParamTuner.tuneWithGridSearch(range_min, range_max, 10))
+
+    print (hyperParamTuner.tuneWithRandomSearch(range_min, range_max, 30))
+    print (hyperParamTuner.tuneWithGridSearch(range_min, range_max, 30))
     print (hyperParamTuner.tuneWithBayesianOptimization(range_min, range_max))
